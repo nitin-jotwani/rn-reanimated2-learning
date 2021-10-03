83390082f8f6c20fa6d23f6141df8c45703bcd4e
Nitin Jotwani <nitin@fancode.com>
2021-10-03 18:51:17 +0530

    update worket

--- src/learning/worklet.js
+++ src/learning/worklet.js
@@ -6,19 +6,24 @@ import { runOnJS, runOnUI } from "react-native-reanimated";
 export const Worklet = () => {
   const runOnUIThread = () => {
     "worklet";
+    console.log("Inside runOnUIThread");
     return "from runOnUIThread worklet";
   };
+
   const runOnJSThread = () => {
+    console.log("Inside runOnJSThread");
     return "from runOnJSThread";
   };
+
   const helloWorketFunc = (from) => {
     "worklet";
     console.log("from helloWorketFunc", from);
     const runOnUIResult = runOnUIThread();
-    const runOnJSResult = runOnJSThread();
+    const runOnJSResult = runOnJSThread(); // will give err, as trying to exe on js on UI
+    // const runOnJSResult = runOnJS(runOnJSThread)(); // will go inside js func but won't return result. As js exe is async & we're in UI thread which sync
     /* 
       Above const will give err, as it's trying to execute on UI thread
-      Since, hellowWorklet is running in UI thread, it is expected invoking other func will be exe in UI thread.
+      Since, hellowWorklet is running in UI thread, it is expected invoking other func will be exe in UI thread. (in docs refer point 3 in information section)
       So, if other func is not marked as 'worklet' will get err.
       Two options to resolve this:
       - Mark func as worklet
